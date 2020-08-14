6029d393bf6b13af9df4ca8e9a49eab36f84565b
przemekbok <przegwibok@gmail.com>
2020-08-15 01:07:23 +0200

    .env configuration for good!

--- server/app.js
+++ server/app.js
@@ -3,18 +3,25 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 mongoose.Promise = global.Promise;
+
+dotenv.config();
+
 if (process.env.NODE_ENV === 'test') {
   mongoose.connect('mongodb://localhost/APIAuthenticationTEST', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 } else {
-  mongoose.connect('mongodb://localhost/APIAuthentication', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(
+    `mongodb://${process.env.LOGIN}:${process.env.PASSWORD}@localhost/APIAuthentication`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 }
 
 const app = express();
